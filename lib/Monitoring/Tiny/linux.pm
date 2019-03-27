1c99557daf593f9c5b1f1ba069b33987479c16a6
Ihab Hussein <ihab@ihabhussein.com>
2019-03-27 11:27:42 +0200

    Remove linux-related code for the time being
    It will be rewritten when a linux machine will be available for testing

--- lib/Monitoring/Tiny/linux.pm
+++ lib/Monitoring/Tiny/linux.pm
@@ -1,37 +1,10 @@
-package Monitor::linux;
+package Monitoring::Tiny;
 use warnings;
 
 if ($^O eq 'linux') {
-    %Monitoring::Tiny::cmds = (
-        TOP => '/usr/bin/top -b -n1 -d1 | grep -v "^ "',
-        DSK => '/bin/df -m',
-    );
-    %Monitoring::Tiny::fields = (
-        '\S+ Mem '          => [
-            MEM => qr/(\d+) total\D+(\d+) free/,
-            [qw(total free)],
-        ],
-        '\S+ Swap'          => [
-            SWP => qr/(\d+) total\D+(\d+) free/,
-            [qw(total free)],
-        ],
-        '%Cpu(s)'           => [
-            CPU => qr/(\d+\.\d+) us.+?(\d+\.\d+) sy.+?(\d+\.\d+) id/,
-            [qw(user sys idle)],
-        ],
-        'Tasks'             => [
-            PRC => qr/(\d+) total\D+(\d+) running\D+(\d+) sleeping/,
-            [qw(total running sleeping)],
-        ],
-        DSK                 => [
-            DSK => qr{^/\S+\s+(\d+)\s+\S+\s+(\d+)[^/]+(/.*)},
-            [qw(total free mount)],
-        ],
-    );
+    sub monitor {
+        my %data = (timestamp => $^T);
 
-    if (-x '/usr/bin/apt') {
-        $Monitoring::Tiny::cmds{PKG} = 'apt list --upgradable | grep -v Listing';
-    }
-};
-
+        return \%data;
+    };
 1;
